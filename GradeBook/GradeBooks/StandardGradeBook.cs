14ec4a8ba21a7e4dec69956645af3af9fee95802
jotazet <jakub.zydron@microsoft.wsei.edu.pl>
2025-03-05 22:19:14 +0100

    Type property added + StandardGradeBook  created

---
+++ GradeBook/GradeBooks/StandardGradeBook.cs
@@ -0,0 +1,18 @@
+﻿using GradeBook.Enums;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Serialization;
+
+namespace GradeBook.GradeBooks
+{
+    class StandardGradeBook : BaseGradeBook
+    {
+        public StandardGradeBook(string name) : base(name)
+        {
+            Type = GradeBookType.Standard;
+        }
+    }
+}
