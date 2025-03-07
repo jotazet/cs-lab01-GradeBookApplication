c3380972a33cdd9865dee0d776999135939739e6
jotazet <jakub.zydron@microsoft.wsei.edu.pl>
2025-03-07 22:21:14 +0100

    Override `RankedGradeBook`'s `GetLetterGrade` method

--- GradeBook/GradeBooks/RankedGradeBook.cs
+++ GradeBook/GradeBooks/RankedGradeBook.cs
@@ -12,5 +12,45 @@ namespace GradeBook.GradeBooks
         {
             Type = GradeBook.Enums.GradeBookType.Ranked;
         }
+
+        public override char GetLetterGrade(double averageGrade)
+        {
+            if (Students.Count < 5)
+            {
+                throw new InvalidOperationException("Requires a minimum of 5 students");
+            }
+
+            int higherGradesCount = 0;
+            foreach (var student in Students)
+            {
+                if (student.AverageGrade > averageGrade)
+                {
+                    higherGradesCount++;
+                }
+            }
+
+            double rangPercent = (double)higherGradesCount / Students.Count;
+
+            if (rangPercent < 0.2)
+            {
+                return 'A';
+            }
+            else if (rangPercent < 0.4)
+            {
+                return 'B';
+            }
+            else if (rangPercent < 0.6)
+            {
+                return 'C';
+            }
+            else if (rangPercent < 0.8)
+            {
+                return 'D';
+            }
+            else
+            {
+                return 'F';
+            }
+        }
     }
 }
